324bacfc367fa73db5923bbc14d3dbb3913aaa46
eve0415 <40632436+eve0415@users.noreply.github.com>
2024-12-02 23:20:36 +0900

    fix: lint/prettier everything

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from "next";
-import type { FC, PropsWithChildren } from "react";
+import type { Metadata } from 'next';
+import type { FC, PropsWithChildren } from 'react';
 
-import "./global.css";
+import './global.css';
 
 export const metadata: Metadata = {
-	title: "フロントエンドコーディング試験",
+  title: 'フロントエンドコーディング試験',
 };
 
 const RootLayout: FC<PropsWithChildren> = ({ children }) => {
-	return (
-		<html lang="ja">
-			<body>
-				<main>{children}</main>
-			</body>
-		</html>
-	);
+  return (
+    <html lang='ja'>
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
 };
 
 export default RootLayout;
