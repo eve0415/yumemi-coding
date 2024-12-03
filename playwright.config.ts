aee5aec8436f6a712223ee976218d6ced6db4e6b
eve0415 <40632436+eve0415@users.noreply.github.com>
2024-12-03 22:26:10 +0900

    chore(e2e): add playwright config

---
+++ playwright.config.ts
@@ -0,0 +1,61 @@
+import { loadEnvConfig } from '@next/env';
+import { defineConfig, devices } from '@playwright/test';
+import { join } from 'node:path';
+import { cwd, env } from 'node:process';
+
+loadEnvConfig(cwd());
+
+const { PORT, DEPLOYMENT_URL } = env;
+
+const baseURL = DEPLOYMENT_URL ?? `http://localhost:${PORT ?? 3000}`;
+
+// Reference: https://playwright.dev/docs/test-configuration
+export default defineConfig({
+  timeout: 30 * 1000,
+  testDir: join(__dirname, 'e2e'),
+  retries: 2,
+  outputDir: 'test-results/',
+  webServer: DEPLOYMENT_URL
+    ? undefined
+    : {
+        command: 'pnpm dev',
+        url: baseURL,
+        timeout: 120 * 1000,
+      },
+
+  use: {
+    baseURL,
+    trace: 'retry-with-trace',
+  },
+
+  projects: [
+    {
+      name: 'Desktop Chrome',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+    {
+      name: 'Desktop Firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+      },
+    },
+    {
+      name: 'Desktop Safari',
+      use: {
+        ...devices['Desktop Safari'],
+      },
+    },
+    {
+      name: 'Mobile Chrome',
+      use: {
+        ...devices['Pixel 5'],
+      },
+    },
+    {
+      name: 'Mobile Safari',
+      use: devices['iPhone 12'],
+    },
+  ],
+});
