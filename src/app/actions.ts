67a0b58c6a17d74c354d2cc3143c327766f5936a
eve0415 <40632436+eve0415@users.noreply.github.com>
2024-12-04 23:05:07 +0900

    feat: implement fetching data

---
+++ src/app/actions.ts
@@ -0,0 +1,62 @@
+'use server';
+
+import { api } from '@/api';
+
+/**
+ * Revalidate every 1 hour
+ */
+const revalidate = 60 * 60;
+
+const PopulationType = {
+  total: '総人口',
+  young: '年少人口',
+  workingAge: '生産年齢人口',
+  elderly: '老年人口',
+} as const;
+
+export const getCompositionPerYear = async (prefCode: number[], populationType: keyof typeof PopulationType) => {
+  const { data: prefectures } = await api.GET('/api/v1/prefectures');
+  if (!prefectures) return null;
+
+  // Remove duplicates & Check if the prefecture code exists
+  // Shouldn't happen normally, but just in case if user tries to manipulate the request
+  const prefCodes = [...new Set(prefCode)];
+  const validCodes = prefectures.result.filter(({ prefCode: code }) => prefCodes.includes(code));
+
+  const results = await Promise.allSettled(
+    validCodes.map(async c => ({
+      ...c,
+      data: await api.GET('/api/v1/population/composition/perYear', {
+        params: { query: { prefCode: `${c.prefCode}` } },
+        next: { revalidate },
+        cache: 'force-cache',
+      }),
+    })),
+  );
+  const result = results
+    .filter(r => r.status === 'fulfilled')
+    .map(({ value }) => (value.data.data?.result ? { code: value.prefCode, name: value.prefName, result: value.data.data.result } : null))
+    .filter(Boolean);
+
+  // Check if boundary year is the same
+  // If not, we would use the oldest boundary year
+  const boundary = result.map(({ result }) => result.boundaryYear).sort()[0];
+  // Probably shouldn't happen, but just in case
+  if (!boundary) return null;
+
+  const data = result.filter(({ result }) => result.boundaryYear === boundary);
+
+  const dataByYear = new Map<number, { year: number; [key: string]: number }>();
+  for (const { name, result } of data) {
+    for (const { year, value } of result.data.find(({ label }) => label === PopulationType[populationType])?.data ?? []) {
+      const existing = dataByYear.get(year);
+      dataByYear.set(year, { year, ...existing, [name]: value });
+    }
+  }
+
+  return {
+    boundaryYear: boundary,
+    codes: data.map(({ code }) => code),
+    data: dataByYear.values().toArray(),
+  };
+};
