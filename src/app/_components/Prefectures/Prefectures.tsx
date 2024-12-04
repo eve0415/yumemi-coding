10d91fc0bb3511389d4f2936e045b4e87ab402f9
eve0415 <40632436+eve0415@users.noreply.github.com>
2024-12-04 21:12:08 +0900

    feat: implement primitive prefecture selection component

---
+++ src/app/_components/Prefectures/Prefectures.tsx
@@ -0,0 +1,18 @@
+import { api } from '@/api';
+import type { FC } from 'react';
+import PrefectureCheckbox from './Checkbox.client';
+
+const Prefectures: FC = async () => {
+  const { data } = await api.GET('/api/v1/prefectures', { cache: 'force-cache', next: { revalidate: 60 * 60 } });
+  if (!data) return null;
+
+  return (
+    <div>
+      {data.result.map(({ prefCode, prefName }) => (
+        <PrefectureCheckbox key={prefCode}>{prefName}</PrefectureCheckbox>
+      ))}
+    </div>
+  );
+};
+
+export default Prefectures;
