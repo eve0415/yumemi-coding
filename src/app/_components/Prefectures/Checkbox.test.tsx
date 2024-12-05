84a345e274d652a73cae574e7d40a8048b19b83d
eve0415 <40632436+eve0415@users.noreply.github.com>
2024-12-05 21:00:31 +0900

    test: add tests

---
+++ src/app/_components/Prefectures/Checkbox.test.tsx
@@ -0,0 +1,53 @@
+import type { getCompositionPerYear } from '@/app/actions';
+import '@testing-library/jest-dom/vitest';
+import { cleanup, render, screen, waitFor } from '@testing-library/react';
+import { afterEach, expect, test, vi } from 'vitest';
+import { ResasViewerProvider } from '../ResasViewer';
+import PrefectureCheckbox from './Checkbox.client';
+
+test('it should be able to clickable', () => {
+  const fn = vi.fn<typeof getCompositionPerYear>(codes => Promise.resolve({ codes, boundaryYear: 2024, data: [] }));
+
+  render(
+    <ResasViewerProvider fetchData={fn}>
+      <PrefectureCheckbox prefCode={1}>TEST1</PrefectureCheckbox>
+    </ResasViewerProvider>,
+  );
+
+  expect(screen.getByLabelText('TEST1')).toBeInTheDocument();
+
+  const checkbox = screen.getByRole('checkbox');
+  expect(checkbox).not.toBeDisabled();
+
+  expect(fn).not.toHaveBeenCalledOnce();
+
+  checkbox.click();
+  expect(checkbox).toBeDisabled();
+  expect(fn).toHaveBeenCalledOnce();
+});
+
+test('it should remove check when api returned without success', async () => {
+  const fn = vi.fn<typeof getCompositionPerYear>(() => Promise.resolve({ codes: [], boundaryYear: 2024, data: [] }));
+
+  render(
+    <ResasViewerProvider fetchData={fn}>
+      <PrefectureCheckbox prefCode={2}>TEST2</PrefectureCheckbox>
+    </ResasViewerProvider>,
+  );
+  screen.debug();
+
+  const checkbox = screen.getByRole('checkbox');
+  expect(checkbox).not.toBeDisabled();
+
+  expect(fn).not.toHaveBeenCalledOnce();
+
+  checkbox.click();
+  expect(checkbox).toBeDisabled();
+
+  await waitFor(() => expect(checkbox).not.toBeDisabled());
+  expect(fn).toHaveBeenCalledOnce();
+});
+
+afterEach(() => {
+  cleanup();
+});
